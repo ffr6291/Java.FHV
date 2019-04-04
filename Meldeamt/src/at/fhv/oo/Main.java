cef68d75d7ec96e420964a1e2fd7996536bb33ee
franz <franz@MSI>
2019-04-04 10:14:53 +0200

    04.04.2019

--- Meldeamt/src/at/fhv/oo/Main.java
+++ Meldeamt/src/at/fhv/oo/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	Person Frau = new Person("Test", "Musterfrau", Gender.Female, "01.03.1997");
 	Person Mann = new Person("Mann", "Mustermann", Gender.Male, "08.08.2005");
 	
-//	Frau.hasMarriagaAge();
+	
 	Mann.status(Mann);
 	Frau.status(Frau);
 	
